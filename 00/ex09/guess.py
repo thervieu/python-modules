d0231a58562457de0d20c2c63ad50a972d2f758d
thervieu <thibault99hervieu@gmail.com>
2022-01-05 09:52:38 +0100

    ok, need to fix one of the first though

---
+++ 00/ex09/guess.py
@@ -0,0 +1,24 @@
+import sys
+from random import randint
+
+if __name__ == "__main__":
+    rand_num = randint(1, 99)
+    guess_count = 0
+    while True:
+        print("What's your guess between 1 and 99?")
+        print(">> ", end='')
+        input_ = input()
+        guess_count += 1
+        try:
+            num = int(input_)
+        except:
+            if input_ == 'exit':
+                print('Goodbye!')
+                sys.exit()
+            print("That's not a number.")
+            continue
+        if num == rand_num:
+            break
+        print("Too high!" if num > rand_num else "Too low!")
+    print('Congratulations, you\'ve got it!')
+    print('You won in {} attempts!'.format(guess_count))
---
+++ 00/ex10/ft_progress.py
@@ -0,0 +1,33 @@
+import sys
+import time
+
+start_time = time.time()
+
+def ft_progress(aList):
+    count = len(aList)
+    def show(j):
+        x = int(20 * j / count)
+        print("ETA: {:.2f}s [{:3d}%][{}>{}] {}/{} | elapsed time {:.2f}".format((count - j)/j*(time.time() - start_time), int(j / count * 100), '='*x, ' '*(20 - x), j, count, (time.time() - start_time)), end='\r')
+    show(1)
+    for i, item in enumerate(aList):
+        yield item
+        show(i+1)
+    print()
+
+if __name__ == "__main__":
+    print("example 1:")
+    listy = range(1000)
+    ret = 0
+    for elem in ft_progress(listy):
+        ret += (elem + 3) % 5
+        time.sleep(0.01)
+    print()
+    print(ret)
+    print("\nexample 2:")
+    listy = range(3333)
+    ret = 0
+    for elem in ft_progress(listy):
+        ret += elem
+        time.sleep(0.005)
+    print()
+    print(ret)
