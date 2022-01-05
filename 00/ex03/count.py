ed00af4508ba04f461063e7cab421903636f6bae
thervieu <thibault99hervieu@gmail.com>
2022-01-05 10:44:15 +0100

    ex04

--- 00/ex03/count.py
+++ 00/ex03/count.py
@@ -1,12 +1,19 @@
 import sys
 from string import punctuation
 
-def text_analyzer(text):
-    if text == "":
-        print("Please input a text")
+def text_analyzer(*args):
+    """Counts the number of upper, lower, punctuation and spaces in the given text"""
+    if len(args) > 1:
+        print("ERROR")
         sys.exit()
+    elif len(args) == 1:
+        text = args[0]
+    else:
+        print("What is the text to analyse?")
+        print(">> ", end='')
+        text = input()
     print("The text contains {} characters".format(len(text)))
     print("- {} upper letters".format(sum(1 for c in text if c.isupper())))
     print("- {} lower letters".format(sum(1 for c in text if c.islower())))
     print("- {} punctuation".format(sum(1 for c in text if c in punctuation)))
-    print("- {} spaces".format(sum(1 for c in text if c == ' ')))
+    print("- {} spaces\n".format(sum(1 for c in text if c.isspace())))
